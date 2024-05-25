a2cdc141549b693463902695ed4077c774bd9146
nitheeshgovind <40054473+nitheeshgovind@users.noreply.github.com>
2024-05-25 05:29:02 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -4,22 +4,7 @@
 // 3. Create a response
 
 // 1. Create a web server
-const http = require('http');
-const server = http.createServer();
-server.listen(3000);
 
-// 2. Create a route
-server.on('request', (req, res) => {
-  if (req.url === '/comments') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify({ comments: [] }));
-    res.end();
-  }
-  else {
-    res.writeHead(404);
-    res.end();
-  }
-});
 // 3. Create a response
 // http://localhost:3000/comments
 // GET /comments
